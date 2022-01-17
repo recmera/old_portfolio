aa976453b5d4b2c415c5e9aee4ab64dffc734c85
rickiwasho <ricardo.coronado@alumnos.uach.cl>
2022-01-16 22:21:21 -0300

    form component

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,18 @@
 import Container from "../components/Container";
-
+import { Heading, Input, Button, Stack, Textarea } from "@chakra-ui/react";
 
 export default function Contact() {
     return (
         <>
-            <h1>Acá irá un formulario de formspree</h1>
+            <Container>
+                <Stack display="flex" spacing={3}>
+                    <Input variant='filled' placeholder='Outline' />
+                    <Textarea variant='filled' placeholder='Filled' />
+                    <Button colorScheme="cyan" size="sm" marginBottom="5%"   >Send</Button>
+                </Stack>
+
+            </Container>
+        
         </>
     )
 }
